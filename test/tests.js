bb7d6fc35260c2b5c0adf91be75ed2ad529326b9
Chris Talkington <chris@christalkington.com>
2012-09-13 10:47:12 -0500

    some quick touchups to tests.

--- test/tests.js
+++ test/tests.js
@@ -6,7 +6,7 @@ module.exports = {
     var zipstream = require('../zipstream');
 
     var hash = crypto.createHash('sha1');
-    var zip = zipstream.createZip({ level: 1 });
+    var zip = zipstream.createZip({level: 1});
 
     // create a buffer and fill it
     var buf = new Buffer(20000);
@@ -15,7 +15,7 @@ module.exports = {
       buf.writeUInt8(i&255, i);
     }
 
-    zip.addFile(buf, {name: 'buffer.out', date: new Date('April 13, 2011 CET') }, function() {
+    zip.addFile(buf, {name: 'buffer.out', date: new Date('April 13, 2011 CET')}, function() {
       zip.finalize();
     });
 
@@ -25,7 +25,7 @@ module.exports = {
 
     zip.on('end', function() {
       var digest = hash.digest('hex');
-      test.equals(digest, '5641d2b95f2cadaabcc22a7d646bfd41036c347d', 'hex values should match.');
+      test.equals(digest, '5641d2b95f2cadaabcc22a7d646bfd41036c347d', 'data hex values should match.');
       test.done();
     });
   },
@@ -37,7 +37,7 @@ module.exports = {
     var zipstream = require('../zipstream');
 
     var hash = crypto.createHash('sha1');
-    var zip = zipstream.createZip({ level: 1 });
+    var zip = zipstream.createZip({level: 1});
 
     // create a buffer and fill it
     var buf = new Buffer(20000);
@@ -46,7 +46,7 @@ module.exports = {
       buf.writeUInt8(i&255, i);
     }
 
-    zip.addFile(buf, {name: 'buffer.out', date: new Date('April 13, 2011 CET'), store: true }, function() {
+    zip.addFile(buf, {name: 'buffer.out', date: new Date('April 13, 2011 CET'), store: true}, function() {
       zip.finalize();
     });
 
@@ -56,7 +56,7 @@ module.exports = {
 
     zip.on('end', function() {
       var digest = hash.digest('hex');
-      test.equals(digest, 'a777c51ca558e9a2ff36f1f9b7fc70b95560df28', 'hex values should match.');
+      test.equals(digest, 'a777c51ca558e9a2ff36f1f9b7fc70b95560df28', 'data hex values should match.');
       test.done();
     });
   }
